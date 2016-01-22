0d4ceb165380d1057fed1ad3631563479206d2e7
brjlewis <brjlewis@ucsc.edu>
2016-01-22 11:02:48 -0800

    Add Ass 1

--- BrandonLewisAssignment1.R
+++ BrandonLewisAssignment1.R
@@ -1,179 +1,102 @@
-###Assigment 1###
-###Brandon Lewis###
-###Winter 2016###
+###(0)###
 
-
-###(0)### 
-
-firstname<-"Brandon"
-lastname<-"Lewis"
-
-print(
-  paste(
-    firstname,
-    lastname
-  )
+BrandonLewisAssignment2<-list(
+  firstname="Brandon",
+  lastname="Lewis",
+  email="brjlewis@ucsc.edu",
+  StudenID=1154992
 )
-
-studentID<-"1154992"
-print(studentID)
-
 
 ###(1)###
 
-df.dta<-read.dta(
-file="https://github.com/EconomiCurtis/econ294_2015/raw/master/data/NHIS_2007_dta.dta")
+library(foreign)
+load(url("https://github.com/EconomiCurtis/econ294_2015/raw/master/data/diamonds.RData"))
 
-View(df.dta)
+BrandonLewisAssignment2$s1a<-nrow(diamonds)
+print("There are 7 observations.")
 
-df.csv<-read.csv(
-file="https://github.com/EconomiCurtis/econ294_2015/raw/master/data/NHIS_2007_CSV.csv")
+BrandonLewisAssignment2$s1b<-ncol(diamonds)
+print("There are 4 rows.")
 
-View(df.csv)
+BrandonLewisAssignment2$s1c<-colnames(diamonds)
+print(BrandonLewisAssignment2$s1c)
 
-df.td<-read.table(
-file="https://github.com/EconomiCurtis/econ294_2015/raw/master/data/NHIS_2007_TSV.txt")
-
-View(df.td)
-
-load(url(
-"https://github.com/EconomiCurtis/econ294_2015/raw/master/data/NHIS_2007_RData.RData"))
-
-View(NHIS_2007_RData)
-
-print("The name assigned to this file is NHIS_2007_RData")
-
+BrandonLewisAssignment2$s1d<-summary(diamonds$price)
+print(BrandonLewisAssignment2$s1d)
 
 ###(2)###
 
-print("The .dta file is 193 KB")
-print("The .csv file is 142 KB")
-print("The .txt file is also 142 KB")
-print("The RData file is 46 KB")
+library(foreign)
+NHIS<-read.table("https://github.com/EconomiCurtis/econ294_2015/raw/master/data/NHIS_2007_TSV.txt",sep="\t", header=TRUE)
 
-print("The RData file is the smallest. The variation in size is due to how compact
-      each data file is. If you look at the data, the files that are larger contain 
-      more spaces, which take up more memory. The RData file has no spaces. This
-      makes it difficult to read, but takes up less memory. The .dta data file takes
-      up the .csv and .txt data files take up more memory but are much easier to read")
+BrandonLewisAssignment2$s2a<-nrow(NHIS)
+print("There are 4785 observations.")
+
+BrandonLewisAssignment2$s2b<-ncol(NHIS)
+print("There are 9 columns.")
+
+BrandonLewisAssignment2$s2c<-colnames(NHIS)
+print(BrandonLewisAssignment2$s2c)
+
+BrandonLewisAssignment2$s2d<-mean(NHIS$weight)
+print("The mean weight is 266.")
+
+BrandonLewisAssignment2$s2e<-median(NHIS$weight)
+print("The median weight is 175.")
+
+hist(NHIS$weight)
+
+table(NHIS$weight)
+
+NHIS$weightadj<-ifelse(NHIS$weight>=996&NHIS$weight<=999,NA,NHIS$weight)
+
+BrandonLewisAssignment2$s2f<-mean(NHIS$weightadj,na.rm=TRUE)
+print("The adjusted mean is 174.")
+
+BrandonLewisAssignment2$s2g<-median(NHIS$weightadj,na.rm=TRUE)
+print("The adjusted median is 170.")
+
+BrandonLewisAssignment2$s2h<-summary(NHIS$weightadj,NHIS$SEX==2)
+print(BrandonLewisAssignment2$s2h)
+
+BrandonLewisAssignment2$s2i<-summary(NHIS$weightadj,NHIS$SEX==1)
+print(BrandonLewisAssignment2$s2i)
 
 ###(3)###
 
-class(NHIS_2007_RData)
-typeof(NHIS_2007_RData)
+vec<-c(letters,LETTERS)
 
-print("The class of this data is data.frame, and the type is list")
+BrandonLewisAssignment2$s3a<-vec[seq(2,52,2)]
+print(BrandonLewisAssignment2$s3a)
 
-length(NHIS_2007_RData)
-dim(NHIS_2007_RData)
-nrow(NHIS_2007_RData)
-ncol(NHIS_2007_RData)
-summary(NHIS_2007_RData)
+BrandonLewisAssignment2$s3b<-paste(vec[c(28,18,1)],collapse="")
+print(BrandonLewisAssignment2$s3b)
 
-print("The length is 9, the dimension is 4785x9, the number
-      of rows is is 4785, the number of columns is 9, and
-      the summary is as follows: 
+arr<-array(c(letters,LETTERS),dim=c(3,3,3))
 
-HHX             FMX             FPX             SEX       
- Min.   :   16   Min.   :1.000   Min.   :1.000   Min.   :1.000  
-      1st Qu.:13404   1st Qu.:1.000   1st Qu.:1.000   1st Qu.:1.000  
-      Median :27527   Median :1.000   Median :1.000   Median :2.000  
-      Mean   :27009   Mean   :1.019   Mean   :1.359   Mean   :1.549  
-      3rd Qu.:40192   3rd Qu.:1.000   3rd Qu.:2.000   3rd Qu.:2.000  
-      Max.   :53955   Max.   :6.000   Max.   :8.000   Max.   :2.000  
-      BMI            SLEEP             educ           height     
-      Min.   :12.91   Min.   : 3.000   Min.   : 0.00   Min.   :59.00  
-      1st Qu.:23.63   1st Qu.: 6.000   1st Qu.:12.00   1st Qu.:64.00  
-      Median :26.97   Median : 7.000   Median :13.00   Median :67.00  
-      Mean   :31.73   Mean   : 9.507   Mean   :14.25   Mean   :69.58  
-      3rd Qu.:31.51   3rd Qu.: 8.000   3rd Qu.:16.00   3rd Qu.:71.00  
-      Max.   :99.99   Max.   :99.000   Max.   :99.00   Max.   :99.00  
-      weight     
-      Min.   :100.0  
-      1st Qu.:149.0  
-      Median :175.0  
-      Mean   :266.2  
-      3rd Qu.:215.0  
-      Max.   :999.0  ")
+BrandonLewisAssignment2$s3c<-arr[1:3,1,2]
+print(BrandonLewisAssignment2$s3c)
 
+BrandonLewisAssignment2$s3d<-arr[2,2,1:3]
+print(BrandonLewisAssignment2$s3d)
+
+BrandonLewisAssignment2$s3e<-paste(arr[2,1,1],arr[3,3,2],arr[1,1,1],sep="")
+print(BrandonLewisAssignment2$s3e)
 
 ###(4)###
 
-df<-read.dta(
-"https://github.com/EconomiCurtis/econ294_2015/raw/master/data/org_example.dta")
+library(foreign)
+Alan<- read.dta("https://github.com/EconomiCurtis/econ294_2015/raw/master/data/org_example.dta")
 
-str(df)
+sort(unique(Alan$year))
+sort(unique(Alan$month))
+sort(unique(Alan$educ))
 
-print("This data set has 1119754 observations and 30 variables")
-
-summary(df$rw)
-
-print(" Min:1.8, 1st Qu:10.7,  Median:15.9, Mean:19.8,
-      3rd Qu:24.4    Max:354.8,    NA's:521279") 
+BrandonLewisAssignment2$s4<-aggregate(Alan$rw,list(Year=Alan$year,Month=Alan$month,Educ=Alan$educ,RW=Alan$rw),mean,na.rm=TRUE)
+print(BrandonLewisAssignment2$s4)
 
 
-###(5)###
 
-v<-c(1,2,3,4,5,6,7,4,NULL,NA)
-        
-length(v)
-
-print("There is a discrepency because NULL is no acknowledged as 
-      a value")
-
-mean(v,na.rm = TRUE)
-
-print("Excluding the NA value, the mean is 4")
-
-
-###(6)###
-
-x<-matrix(c(1,2,3,4,5,6,7,8,9),ncol=3,byrow=TRUE)
-
-t(x)
-
-eign<-eigen(x)
-
-eign$vec
-eign$val
-
-
-y<-matrix(c(1,2,3,3,2,1,2,3,0),ncol=3,byrow=TRUE)
-
-solve(y)
-
-y%*%solve(y)
-
-print("The resulting matrix is an identity matrix")
-
-
-###(7)###
-
-carat<-c(5,2,0.5,1.5,5,NA,3)
-cut<-c("fair","good","very good","good","fair","Ideal","fair")
-clarity<-c("SI1","I1","VI1","VS1","IF","VVS2",NA)
-price<-c(850,450,450,NA,750,980,420)
-
-dmnd<-data.frame(carat,cut,clarity,price)
-mean(dmnd$price,na.rm=TRUE)
-
-print("The mean of price is 650")
-
-faircut<-subset(dmnd,cut=="fair")
-mean(faircut$price,na.rm=TRUE)
-
-print("The mean price of the fair cuts is 673.3333")
-
-othercuts<-subset(dmnd,cut!="fair")
-mean(othercuts$price,na.rm=TRUE)
-
-print("The mean price of the good, very good and ideal cuts is 626.6667")
-
-large<-subset(dmnd,carat>2&cut=="Ideal"|carat>2&cut=="very good")
-median(large$price,na.rm=TRUE)
-
-print("There is no ideal or very good diamonds that are over 2 carats,
-      so the median is not available")
 
 
 
